f3368938f923fdfaa1e955cf373ad66740e42ccb
Caleb Payán Inzunza <calebpayan75@gmail.com>
2023-10-11 12:55:50 -0700

    Front updated
    SpeechHome as the main page route = /#/home

--- front/src/components/HelpWindow.js
+++ front/src/components/HelpWindow.js
@@ -2,9 +2,17 @@ import React from 'react'
 
 function HelpWindow() {
   return (
-    <div className='h-screen w-full bg-yellow-400'>
+    <div className='h-screen w-full bg-yellow-400 flex items-center justify-center'>
 
+      <div className='h-3/4 flex-col justify-between'>
+        <div className='flex justify-center'>
+          <h1 className='text-yellow-900 font-black text-5xl'>Ayuda</h1>
+        </div>
 
+        <div>
+
+        </div>
+      </div>
 
     </div>
   )
